4cd1f5c3b431d80c59b40a2fc7cac4e3e5535450
Ameet Pradhan <ameetp@dewsolutions.in>
2023-07-13 15:15:19 +0530

    added babel rollup plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,40 +1,38 @@
-import resolve from "@rollup/plugin-node-resolve";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
-
+import babel from "@rollup/plugin-babel";
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
     {
         input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
+        output: {
+            file: packageJson.module,
+            format: "esm",
+            sourcemap: true,
+        },
         plugins: [
-            resolve(),
+            nodeResolve(),
             commonjs(),
+            babel({
+                babelHelpers: "bundled",
+                exclude: "node_modules/**",
+            }),
             typescript({ tsconfig: "./tsconfig.json" }),
             terser(), //Minify the output file
+
         ],
         external: ["react"],
     },
     {
-        input: "dist/esm/types/index.d.ts",
+        input: "src/index.ts",
         output: [
             {
                 file: "dist/index.d.ts",
-                format: "esm",
+                format: "es",
             },
         ],
         plugins: [dts()],
